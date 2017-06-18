eaeaf4d3fc986a69e721235033749a04b21c8253
BadgeSWS <jackson.brajer@gmail.com>
2017-06-18 00:04:27 -0400

    Added Bishop Logic

--- core/src/pieces/Piece.java
+++ core/src/pieces/Piece.java
@@ -1,6 +1,14 @@
 package pieces;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.ChessGame;
+import screens.GameScreen;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jacks on 2017-06-17.
@@ -12,6 +20,8 @@ public class Piece {
     private String type;
     private int row;
     private int col;
+    private int x;
+    private int y;
 
 
     public Piece(String color, String type, int row, int col) {
@@ -20,6 +30,8 @@ public class Piece {
         this.type = type;
         this.row = row;
         this.col = col;
+        this.x = colToX(col);
+        this.y = rowToY(row);
         if(type.equals("KING"))
             if(getColor().equals("BLACK"))
                 img = new Texture("pieces/blackKnight.png");
@@ -52,6 +64,75 @@ public class Piece {
                 img = new Texture("pieces/whiteKnight.png");
     }
 
+    public boolean canBishopMove(int row, int col){
+        boolean isDiagonal;
+        int diffCol = Math.abs(this.row - row);
+        int diffEow = Math.abs(this.col - col);
+        isDiagonal = diffCol == diffEow;
+
+        boolean isAPeiceInThePath = false;
+
+        if(isDiagonal){
+            boolean goingUp = this.row > row;
+            boolean goingLeft = this.col > col;
+
+            int colMod = goingLeft?-1:1;
+            int rowMod = goingUp?-1:1;
+            Vector2 vec2ModOff = new Vector2(0, 0);
+            List<Vector2> coords = new ArrayList<Vector2>();
+            do{
+                coords.add(new Vector2(this.col+vec2ModOff.x,this.row+vec2ModOff.y));
+                vec2ModOff.x += colMod;
+                vec2ModOff.y += rowMod;
+            } while(coords.get(coords.size()-1).x != col && coords.get(coords.size()-1).y != row);
+
+            int pieceToRemove = -1;
+            for(int i = 1; i < coords.size(); i++){
+                Vector2 coord = coords.get(i);
+                for(int x = 0; x < GameScreen.getPieces().size(); x++){
+                    Piece p = GameScreen.getPieces().get(x);
+                    if (p.getCol() == coord.x && p.getRow() == coord.y) {
+                        if(i == coords.size()-1){
+                            if(!p.getColor().equals(getColor())) {
+                                pieceToRemove = x;
+                            } else
+                                isAPeiceInThePath = true;
+                        } else
+                            isAPeiceInThePath = true;
+                    }
+                }
+            }
+            if(pieceToRemove != -1)
+                GameScreen.getPieces().remove(pieceToRemove);
+            pieceToRemove = -1;
+
+        }
+
+        return isDiagonal && !isAPeiceInThePath;
+    }
+
+    public boolean canMove(int row, int col){
+        if(type.equals("BISHOP")) {
+            return canBishopMove(row, col);
+        } else if (type.equals("PAWN")){
+            return true;
+        }
+        return false;
+    }
+
+    public void movePiece(int row, int col){
+        if(canMove(row,col)) {
+            this.row = row;
+            this.col = col;
+        }
+        centerOnGrid();
+    }
+
+    public void centerOnGrid(){
+        setX(colToX(col));
+        setY(rowToY(row));
+    }
+
     public Texture getImg() {
         return img;
     }
@@ -72,4 +153,35 @@ public class Piece {
         return col;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setCol(int col){
+        this.col = col;
+    }
+
+    public void setRow(int row){
+        this.row = row;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int rowToY(int row){
+        return ChessGame.HEIGHT - ((row+1)*90);
+    }
+
+    public int colToX(int col){
+        return col*90;
+    }
 }
--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -7,7 +7,12 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.ChessGame;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.input.PickResult;
 import pieces.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jacks on 2017-06-16.
@@ -16,9 +21,11 @@ public class GameScreen implements Screen{
 
     public static final int SPEED = 400;
 
-    Texture board;
-    ChessGame game;
-    Piece[][] pieces = new Piece[8][8];
+    private Texture board;
+    private ChessGame game;
+    private static List<Piece> pieces = new ArrayList<Piece>();
+    private int elementMoved;
+    private int oldX, oldY;
 
     public GameScreen(ChessGame game){
         this.game = game;
@@ -26,7 +33,29 @@ public class GameScreen implements Screen{
 
     @Override
     public void show() {
+        elementMoved = -1;
         board  = new Texture("board.png");
+        pieces.add(new Piece("BLACK", "ROOK", 0, 0));
+        pieces.add(new Piece("BLACK", "KNIGHT", 0, 1));
+        pieces.add(new Piece("BLACK", "BISHOP", 0, 2));
+        pieces.add(new Piece("BLACK", "QUEEN", 0, 3));
+        pieces.add(new Piece("BLACK", "KING", 0, 4));
+        pieces.add(new Piece("BLACK", "BISHOP", 0, 5));
+        pieces.add(new Piece("BLACK", "KNIGHT", 0, 6));
+        pieces.add(new Piece("BLACK", "ROOK", 0, 7));
+        for(int col = 0; col < 8; col++)
+            pieces.add(new Piece("BLACK", "PAWN", 1, col));
+
+        pieces.add(new Piece("WHITE", "ROOK", 7, 0));
+        pieces.add(new Piece("WHITE", "KNIGHT", 7, 1));
+        pieces.add(new Piece("WHITE", "BISHOP", 7, 2));
+        pieces.add(new Piece("WHITE", "QUEEN", 7, 3));
+        pieces.add(new Piece("WHITE", "KING", 7, 4));
+        pieces.add(new Piece("WHITE", "BISHOP", 7, 5));
+        pieces.add(new Piece("WHITE", "KNIGHT", 7, 6));
+        pieces.add(new Piece("WHITE", "ROOK", 7, 7));
+        for(int col = 0; col < 8; col++)
+            pieces.add(new Piece("WHITE", "PAWN", 6, col));
     }
 
     @Override
@@ -34,11 +63,43 @@ public class GameScreen implements Screen{
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if(Gdx.input.isTouched())
-            System.out.println(Gdx.input.getX()/90 + ", " + Gdx.input.getY()/90);
+        if(Gdx.input.isTouched()){
+            if(elementMoved == -1) {
+                oldX = Gdx.input.getX();
+                oldY = ChessGame.HEIGHT - Gdx.input.getY();
+                int row, col;
+                row = Gdx.input.getY()/90;
+                col = Gdx.input.getX()/90;
+
+                for (int i = 0; i < pieces.size(); i++) {
+                    if (pieces.get(i).getRow() == row && pieces.get(i).getCol() == col) {
+                        elementMoved = i;
+                    }
+                }
+            } else {
+                Piece piece = pieces.get(elementMoved);
+                int changeInX = Gdx.input.getX() - oldX;
+                int changeInY = (ChessGame.HEIGHT - Gdx.input.getY()) - oldY;
+                piece.setX(piece.getX() + changeInX);
+                piece.setY(piece.getY() + changeInY);
+
+                oldX = Gdx.input.getX();
+                oldY = ChessGame.HEIGHT - Gdx.input.getY();
+            }
+        } else {
+            if(elementMoved != -1) {
+                Piece piece = pieces.get(elementMoved);
+                piece.movePiece(Gdx.input.getY() / 90, Gdx.input.getX() / 90);
+            }
+            elementMoved = -1;
+        }
 
         game.getBatch().begin();
         game.getBatch().draw(board, 0, 0, ChessGame.WIDTH, ChessGame.HEIGHT);
+        for(int i = 0; i < pieces.size(); i++){
+            Piece piece = pieces.get(i);
+            game.getBatch().draw(piece.getImg(), piece.getX(), piece.getY(), 90,90);
+        }
         game.getBatch().end();
     }
 
@@ -65,5 +126,9 @@ public class GameScreen implements Screen{
     @Override
     public void dispose() {
 
+    }
+
+    public static List<Piece> getPieces() {
+        return pieces;
     }
 }
